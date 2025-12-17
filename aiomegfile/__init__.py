02fc2a7f37f8f2dfaebc735fbc6bc21e5b198401
penghongyang <phy@dexmal.com>
2025-12-17 10:41:35 +0000

    chore:remove __version__ from __all__

--- aiomegfile/__init__.py
+++ aiomegfile/__init__.py
@@ -1,11 +1,10 @@
-from aiomegfile.__version__ import __version__
+from aiomegfile.__version__ import __version__  # noqa: F401
 from aiomegfile.filesystem import (
     LocalFileSystem,
 )
 from aiomegfile.smart_path import SmartPath
 
 __all__ = [
-    "__version__",
     "SmartPath",
     "LocalFileSystem",
 ]
