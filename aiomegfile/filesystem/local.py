7ded35a1d8168cde7795aa634abb53d97c68ee25
penghongyang <phy@dexmal.com>
2026-02-28 09:18:38 +0000

    fix:fix static error

--- aiomegfile/filesystem/local.py
+++ aiomegfile/filesystem/local.py
@@ -283,7 +283,7 @@ class LocalFileSystem(BaseFileSystem):
                     path=path,
                     stat=StatResult(
                         st_size=stat_result.st_size,
-                        st_ctime=getattr(stat_result, "st_ctime", 0.0),
+                        st_ctime=stat_result.st_ctime,  # pyre-ignore[16]
                         st_mtime=stat_result.st_mtime,
                         isdir=stat.S_ISDIR(stat_result.st_mode),
                         islnk=stat.S_ISLNK(stat_result.st_mode),
